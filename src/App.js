f926697a885e0cb27d7705446e11209c971c4f02
emcdanie <66241957+emcdanie@users.noreply.github.com>
2021-07-25 18:49:57 +0200

    audit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import logo from "./logo.png";
 import './App.css';
+import Dictionary from './Dictionary.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+      <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <main>
+        <Dictionary/>
+      </main>
+</header>
+    <footer className="App-footer">
+      <small>This project was coded by{""}
+          <a 
+          href="https:/elletamcdaniel.com" 
+          target="_blank">
+            Elleta Michell Moire McDaniel
+          </a>{""}
+          and is{" "}
+          <a
+            href="https://github.com/emcdanie"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://romantic-banach-8cf215.netlify.app"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a> </small>
+
+    </footer>
     </div>
+    
   );
 }
 
