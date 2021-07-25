889d225e93c655d1fb4090fd23118f2322589b00
emcdanie <66241957+emcdanie@users.noreply.github.com>
2021-07-25 20:31:57 +0200

    syntax errors

--- src/App.js
+++ src/App.js
@@ -4,39 +4,26 @@ import Dictionary from './Dictionary.js';
 
 function App() {
   return (
-    <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
-      <main>
-        <Dictionary/>
-      </main>
-</header>
-    <footer className="App-footer">
-      <small>This project was coded by{""}
-          <a 
-          href="https:/elletamcdaniel.com" 
+     <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer className="App-footer">
+          <small>This project was coded by{""}</small>
+           <a href="https:/elletamcdaniel.com" 
           target="_blank" rel="noreferrer">
             Elleta Michell Moire McDaniel
-          </a>{""}
-          and is{" "}
-          <a
-            href="https://github.com/emcdanie"
-            target="_blank" rel="noreferrer">
-          
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="/"
-            target="_blank>
-          
-            hosted on Netlify
-          </a> </small>
-
-    </footer>
+          </a>
+          <a href="https://github.com/emcdanie"
+            target="_blank" rel="noreferrer">Open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div>
-    
   );
-}
+} 
 
 export default App;
